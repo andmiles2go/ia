36c4d3c6914ff32fb0610c675964215fb6ee7f33
john smith <john_smith_at_jp@163.com>
2016-10-02 20:39:19 +0800

    add first test file for ia project

---
+++ firstAdd.py
@@ -0,0 +1,16 @@
+# /bin/python
+
+import sys
+import string
+
+def jieCheng(k):
+    res = 1
+    while(k != 0):
+	res = k * res
+        k = k - 1
+    return res
+
+if __name__ == '__main__':
+    para = sys.argv[1]
+    r = jieCheng(string.atoi(para))
+    print(r)
